70414171259ebb2c660aa15fbb4e2d967ad55415
JoseLuisMaya <joseluis.mayaquiros@gmail.com>
2017-01-10 23:41:49 +0100

    Primer commit

---
+++ Workspace/PrimerRepositorio/src/suma.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class suma {
+
+	public int SumaContador(){
+		int i = 2;
+		i=i+4;
+		return i;
+	}
+	
+	@Test
+	public int sum(int i, int j){
+		return i+j;
+	}
+}
